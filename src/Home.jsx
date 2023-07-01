c3e4b3409cfc81935488072b8a52a20e20314eb6
hilina-mastewal <hilina>
2023-07-01 19:19:49 +0300

    Added new feature

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 const  Home=()=> {
  
   return (
-    <div className=" bg-[#0f1b34]  absolute top-10  md:left-6  w-full h-full flex items-center  justify-center ">
+    <div className=" bg-[#0f1b34]  absolute top-10  md:left-6  w-full h-full flex items-center  justify-center  overflow-y-scroll ">
       <div className="flex flex-col  justify-center    w-[90vh] md:gap-5 ml-6  ">
   <div className="text-md font-medium text-[#98f4f4] flex items-start  ">Hi My name is,</div>
   <div className="   text-[45px] md:text-6xl font-bold text-[#d8dffe] ">Hilina Mastewal</div>
