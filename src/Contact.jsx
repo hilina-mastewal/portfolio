aae840607132f3acf762d75f844298a406624351
hilina-mastewal <hilina>
2023-07-02 17:34:41 +0300

    Added new feature in all

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -43,7 +43,7 @@ const  Contact=()=> {
     <div className=" xs:text-2xl md:text-2xl mt-6 text-base px-1  font-thin   text-[#80b3cb] xs::w-3/4">  If you would like  me to work for your company or you want me 
    to make you a website ,please put your name, email address and your message.Then i will get back to you within few days.
    </div></div>
-   <div className="flex flex-col   md:mt-10  text-[#b8bed8] text-xl ">
+   <div className="flex-col   md:mt-10 hidden xs:flex text-[#b8bed8] text-xl ">
     <div className="flex gap-1 xs:text-2xl">
 <a><HiOutlineMail  className=" xs:text-base text-xl text-[#b8bed8] mt-1 " size='20'/></a>
 <lable className="contactlist  text-[#d8dffe] text-xs ">Email</lable>
@@ -59,7 +59,7 @@ const  Contact=()=> {
 <lable className=" font-thin md:text-base xs:text-2xl text-base text-[#80b3cb]">Addis Ababa, Ethiopia</lable>
 </div>
     </div>
-    <div className="md:w-[50%] h-1/2 w-full flex flex-col  md:mt-0 md:items-start  items-center xs:h-full ">
+    <div className="md:w-[50%] h-1/2 w-full flex flex-col  md:mt-0 md:items-start h-1/2  items-center xs:h-full ">
  <div className=" xs:text-6xl   text-3xl  font-bold text-[#80b3cb] md:mt-10 flex  md:item-start items-center gap-4  ">
       <h3 className=" xs:text-6xl  md:text-5xl text-3xl ">Just say Hello!</h3>
     <GiHand  className=" mt-2 animate-waving-hand " size='34'/>
