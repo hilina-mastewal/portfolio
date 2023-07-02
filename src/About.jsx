a628ad28dd4048f5b44923ac5eac09acc6b6a779
hilina-mastewal <hilina>
2023-07-02 17:31:53 +0300

    Added new feature in all

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,8 @@
 const  About=()=> {
  
   return (
-    <div className=" bg-[#0f1b34]  flex flex-col md:flex-row items-center justify-center absolute top-10 w-full  h-full dark:bg-slate-100 overflow-y-scroll">
-      <div className="md:w-[40%] xssm:h-[35%]  h-[30%]  xs:h-auto w-[85%] flex  justify-start md:items-center  items-start hover:scale-105 duration-150">
+    <div className=" bg-[#0f1b34]  flex flex-col gap-10 md:flex-row items-center justify-start md:justify-center absolute top-10 w-full  h-full dark:bg-slate-100 overflow-y-scroll">
+      <div className="md:w-[40%] xssm:h-[35%] xs:h-auto w-[85%] flex  md:justify-center justify-start md:items-center  items-start hover:scale-105 duration-150">
         <div className="flex  justify-start xssm:justify-center items-start gap-5 md:gap-0 mt-5 0 relative ">
          <div className="xs:w-10 xs:h-10 w-5 h-5 border-[#98f4f4]   xs:border-l-[6px] border-l-[3px] border-t-[3px] xs:border-t-[6px] left-0 top-0  hover:scale-105 hover:duration-200 "></div>
     <img
@@ -12,20 +12,19 @@ const  About=()=> {
            <div className="xs:w-10 xs:h-10 w-5 h-5 border-[#98f4f4] border-r-[3px] border-b-[3px] xs:border-r-[6px]  xs:border-b-[6px] hover:scale-105 hover:duration-200 right-0 bottom-0 absolute"></div>
          </div>
         </div>
-  <div className=" md:w-[50%] md:h-auto flex flex-col justify-center items-center   md:py-28  md:items-start xssm:gap-10 gap-3 ">
+  <div className=" md:w-[50%] md:h-[70%] flex flex-col justify-center items-center   md:py-28  md:items-start xssm:gap-10 gap-3 ">
 <div className=" text-3xl xs:text-5xl md:text-5xl  font-bold text-[#d8dffe]   ">About Me</div>
-<div className="xs:text-xl flex items-center justify-center  text-xs font-light text-[#80b3cb] space-x-2 px-2 md:ml-0 w-11/12 md:w-4/5 ">
-I am  a senior student at HilCoE (School of computer science an dtechnoloy), I have gained valuable knowledge and experience in various subjects related to my field of study. Additionally, I work as a freelance web designer. While being proficient in my current skill set, I am always eager to expand my horizons and learn new technologies and techniques that can enhance my capabilities as a web designer. I believe in continuously improving myself and staying up-to-date with the latest trends and advancements in the industry to provide the best solutions to my clients.
+<div className="xs:text-xl flex items-center justify-center  text-[0.7rem] font-light text-[#80b3cb] space-x-2  md:ml-0 w-11/12 md:w-4/5 ">
+I am  a senior student at HilCoE, I have gained valuable knowledge and experience in various subjects related to my field of study. Additionally, I work as a freelance web designer. While being proficient in my current skill set, I am always eager to expand my horizons and learn new technologies and techniques that can enhance my capabilities as a web designer. I believe in continuously improving myself and staying up-to-date with the latest trends and advancements in the industry to provide the best solutions to my clients.
   </div>
   <div className="xs:text-2xl text-sm font-light  flex flex-col text-[#80b3cb] space-x-2 w-4/5 ">
-    <div className="flex gap-5">  <p className="text-white pl-2">Birthday</p> <p className="text-[0.8rem] xs:text-xl">April 21 200</p> </div>
 <div className="flex gap-5"> <p className="text-white">Phone</p> 	<p className="text-[0.8rem] xs:text-xl">+251 963-13-02-18</p></div>
  <div className="flex gap-5"><p className="text-white"> Email</p> <p className="text-[0.8rem] xs:text-xl"> Hilina.mastewal.ue6602@gmail.com</p></div>
  <div className="flex gap-5"><p className="text-white">Address</p> <p className="text-[0.8rem] xs:text-xl">Addis Ababa, Ethiopia</p> </div>
  
   </div>
   
-  <a href="hilinasResume.pdf" className="button flex items-center justify-center xs:text-sm text-xs xs:w-1/3  w-6/12 md:w-auto md:px-10">DownLoad Resume</a>
+  <a href="hilinasResume.pdf" className="button flex items-center justify-center xs:text-sm text-xs xs:w-1/3  md:w-auto md:px-10">DownLoad Resume</a>
  
    </div>
 
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -35,31 +35,31 @@ const  Contact=()=> {
 };
 
   return (
-    <div className="bg-[#0f1b34] flex  flex-col  md:flex-row justify-around w-full absolute top-10  md:left-6 h-full overflow-y-scroll">
-  <div className="md:w-[55%] h-1/2 xs:mt-10 md:h-full md:mt-20 flex flex-col xs:pt-[10%] md:pt-0 md:items-start gap-5 md:pl-20  pl-4">
+    <div className="bg-[#0f1b34] flex  flex-col gap-10 md:flex-row items-start justify-around w-full absolute top-10  md:left-6 h-full  overflow-y-scroll">
+  <div className="md:w-[55%]  h-1/2 xs:mt-10 md:h-full md:mt-20  flex flex-col xs:pt-[6%] md:pt-0 md:items-start gap-5 md:pl-20  pl-4">
      <div>
 
-    <div className=" xs:text-5xl text-3xl  md:text-3xl font-light text-[#d8dffe] mt-[15%] xs:mt-10 ">Contact Info</div>
+    <div className=" xs:text-5xl text-3xl  md:text-3xl font-light text-[#d8dffe] mt-5 xs:mt-10 ">Contact Info</div>
     <div className=" xs:text-2xl md:text-2xl mt-6 text-base px-1  font-thin   text-[#80b3cb] xs::w-3/4">  If you would like  me to work for your company or you want me 
    to make you a website ,please put your name, email address and your message.Then i will get back to you within few days.
    </div></div>
    <div className="flex flex-col   md:mt-10  text-[#b8bed8] text-xl ">
     <div className="flex gap-1 xs:text-2xl">
 <a><HiOutlineMail  className=" xs:text-base text-xl text-[#b8bed8] mt-1 " size='20'/></a>
-<lable className="contactlist  text-[#d8dffe] ">Email</lable>
+<lable className="contactlist  text-[#d8dffe] text-xs ">Email</lable>
 </div>
 <lable className="text-[#80b3cb] md:text-base xs:text-2xl text-base  font-thin ">Hilina.Mastewal.Ue6602@gmail.com</lable>
 <div className="flex gap-1">
 <a><HiOutlinePhone  className=" text-xl xs:text-base text-[#b8bed8] mt-1  " size='20'/></a>
-<lable className="contactlist text-[#d8dffe] ">Phone</lable></div>
+<lable className="contactlist text-[#d8dffe] text-xs">Phone</lable></div>
 <lable className=" text-[#80b3cb] md:text-base  text-base xs:text-2xl   font-thin">+251 963-13-02-18</lable>
 <div className="flex gap-1">
 <a><FaRegAddressBook  className=" text-sm xs:text-base text-[#b8bed8] mt-1  " size='20'/></a>
-<lable className="contactlist text-[#d8dffe]  ">Address</lable></div>
+<lable className="contactlist text-[#d8dffe] text-xs ">Address</lable></div>
 <lable className=" font-thin md:text-base xs:text-2xl text-base text-[#80b3cb]">Addis Ababa, Ethiopia</lable>
 </div>
     </div>
-    <div className="md:w-[50%] w-full  flex flex-col h-1/2 mt-10 md:mt-0 md:items-start  items-center xs:h-full">
+    <div className="md:w-[50%] h-1/2 w-full flex flex-col  md:mt-0 md:items-start  items-center xs:h-full ">
  <div className=" xs:text-6xl   text-3xl  font-bold text-[#80b3cb] md:mt-10 flex  md:item-start items-center gap-4  ">
       <h3 className=" xs:text-6xl  md:text-5xl text-3xl ">Just say Hello!</h3>
     <GiHand  className=" mt-2 animate-waving-hand " size='34'/>
@@ -105,7 +105,7 @@ const  Contact=()=> {
         </textarea>
         
         <button
-          className="button w-28 my-4 mb-28 md:w-48 "
+          className="button text-sm my-2 mb-20 md:w-48 "
           type="submit">
           Submit
         </button>
